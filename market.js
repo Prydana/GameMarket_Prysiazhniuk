98c0ae6d90d916501904b81f1da3b51699375e00
Prydana <148072238+Prydana@users.noreply.github.com>
2024-05-08 08:31:42 +0300

    Update market.js

--- market.js
+++ market.js
@@ -11,10 +11,11 @@ if(itemsDiv){
     console.log('ClientWidth:', itemsDiv.clientWidth);
     console.log('InnerHTML:', itemsDiv.innerHTML);
     //Add our products
-    itemsDiv.innerHTML = '<div class="item"></div>';
-    itemsDiv.innerHTML += '<div class="item"></div>';
-    itemsDiv.innerHTML += '<div class="item"></div>';
-    itemsDiv.innerHTML += '<div class="item"></div>';
+    for( let i=0; i<=101; i++) {
+        itemsDiv.innerHTML += '<div class="item"></div>';
+    }
+    
+    
 } else{
     //No
     console.log('Not Found');
